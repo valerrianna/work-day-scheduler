8d9d9b25ccf98eb1b1019651ef65f5a591eda90b
Valerie <valerie.tram.anh.n@gmail.com>
2021-09-25 18:58:11 -0400

    add time to html

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// add real time to calendar to id current day - using moment.js
+
+// add timeblock for standard business hours -> to do in the html file -> edit styling via bootstrap
+//add a div for time 8am to 5pm
+//add text area
+//add save button , inco via bootstrap
+
+//add color coding for each block, current hour in red and future hours in green
+// if time = to current time make text block red
+// else if = time already pass , make block standard color grey
+// else = time is in the future, make it green
+
+//clicking a box allow entry of to do
+// add a function that allows us to click the text box and write something
+
+//save button save the info into local storage
+// add a function that stores written stuff in local storage
+// add ability to load save data from local storage
